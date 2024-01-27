025cf2b1351f5b258e1e3faf87c714adafec9b11
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-01-27 19:29:30 +0530

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import {FcLike,FcLikePlaceholder} from 'react-icons/fc'
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Card(props) {
-  let course=props.course
-  let likedCourses=props.likedCourses
-  let setLikedCourses=props.setLikedCourses
-  function clickHandler(){
-    if(likedCourses.includes(course.id)){
-      setLikedCourses((prev)=>prev.filter((cid)=>(cid!==course.id)))
+  let course = props.course
+  let likedCourses = props.likedCourses
+  let setLikedCourses = props.setLikedCourses
+  function clickHandler() {
+    if (likedCourses.includes(course.id)) {
+      setLikedCourses((prev) => prev.filter((cid) => (cid !== course.id)))
       toast.warning("like removed")
-      
+
     }
-    else{
-      if(likedCourses.lenght===0){
+    else {
+      if (likedCourses.lenght === 0) {
         setLikedCourses([course.id])
         console.log(likedCourses)
 
       }
-      else{
-        setLikedCourses((prev)=>[...prev,course.id])
-        
+      else {
+        setLikedCourses((prev) => [...prev, course.id])
+
 
       }
       toast.success("liked successfully")
@@ -29,32 +29,32 @@ export default function Card(props) {
 
   }
   return (
-    <div className='w-[300px] bg-bgDark bg-opacity-80 rounded-mdmd-overflow-hidden'>
+    <div className='w-[300px] bg-[#22223BCC] bg-opacity-80 rounded-mdmd-overflow-hidden'>
       <div className='relative'>
-              <img src={course.image.url} alt='noting'/>
+        <img src={course.image.url} alt='noting' />
 
-      
-              <div className='w-[20px] h-[20px] bg-white rounded-full absolute right-1 bottom-3
+
+        <div className='w-[20px] h-[20px] bg-white rounded-full absolute right-1 bottom-3
       grid place-items-center'
-      >
-              <button onClick={clickHandler}>
-                
-                {
-                  likedCourses.includes(course.id) ? <FcLike fontSize='1.2rem '/> :<FcLikePlaceholder fontSize='1.2rem ' />
+        >
+          <button onClick={clickHandler}>
 
-                } 
-              </button>
-              </div>
+            {
+              likedCourses.includes(course.id) ? <FcLike fontSize='1.2rem ' /> : <FcLikePlaceholder fontSize='1.2rem ' />
+
+            }
+          </button>
+        </div>
       </div>
-      
+
 
       <div className='p-4'>
         <p className='text-white font-semibold text-lg leading-6'>{course.title}</p>
         <p className='mt-2 text-white'>{
-          course.description.length>100 ? (course.description.substr(0,100)+"....."): (course.description)
+          course.description.length > 100 ? (course.description.substr(0, 100) + ".....") : (course.description)
 
         }
-        
+
         </p>
       </div>
 
